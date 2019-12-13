e2ed2388c8b2bd4eaf5d37b7e3d5f1faadf8aabb
Cody Pellom <cpellom@appliedsystems.com>
2019-12-12 20:19:19 -0500

    'n'

---
+++ main.js
@@ -0,0 +1,11 @@
+var c = document.getElementById("c");
+var a = document.getElementById("a");
+var m = document.getElementById("m");
+var p = document.getElementById("p");
+
+function cLights(e) {
+   if ("mouseover") {a.style.color = "white";})  function(){
+        
+
+    };
+}
